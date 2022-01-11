ff2c8013d65c56db4a96395d94c75d5289416a5b
crazylawn <devv1231@gmail.com>
2022-01-11 16:16:39 +0900

    ADD : char 3  => number, string , null , undefined

---
+++ js-study/pages/dataType/operator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const Operator: NextPage = () => {
+  return (
+    <div>
+      3-1.자바스트립트 기본 타입
+      <p>숫자 </p>
+      <p>문자열 </p>
+      <p>불린값 </p>
+      <p>null 과 undefined</p>
+    </div>
+  );
+};
+
+export default Operator;
